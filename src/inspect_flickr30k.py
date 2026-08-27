f6e09d0106e2a53684b2f9ac04a5a0d555dbeb07
scwang <shuchangwang83@gmail.com>
2026-08-27 16:56:24 +0800

    chore：查看数据情况脚本

---
+++ src/inspect_flickr30k.py
@@ -0,0 +1,33 @@
+import os
+from datasets import load_dataset
+
+storage_root=f"/data/{os.environ['USER']}/vlm_learning"
+dataset_cache=f"{storage_root}/datasets/flickr30k"
+
+print("loading datasets")
+
+dataset=load_dataset(
+    "nlphuji/flickr30k",
+    split="test",
+    cache_dir=dataset_cache,
+    trust_remote_code=True,
+)
+print("数据集对象：", dataset)
+print("数据量：", len(dataset))
+print("字段名称：", dataset.column_names)
+
+sample=dataset[0]
+
+for key,value in sample.items():
+    if isinstance(value,list):
+        print("列表长度：" ,len(value))
+        print("列表前两项：",value[:1])
+    else:
+        print("内容：", value)
+
+image = sample["image"]
+
+print("\n图片信息：")
+print("图片类型：", type(image))
+print("图片尺寸：", image.size)
+print("图片模式：", image.mode)
